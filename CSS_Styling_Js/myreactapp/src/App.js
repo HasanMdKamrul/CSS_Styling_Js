3fc4a56810a84204593931b50c448f18ad0c2148
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-06-18 03:32:09 +0200

    Initial commit and global css import technique

---
+++ CSS_Styling_Js/myreactapp/src/App.js
@@ -0,0 +1,14 @@
+// ** Global import of css must include the .css at the end 
+// ** This will allow us to just write down the class name and no {} object syntax needed here!
+
+import './assets/css/global.css';
+import Logo from './components/Logo';
+
+export default function App() {
+    return (
+        <div className="App">
+            <h1>Learn with Sumit</h1>
+            <Logo />
+        </div>
+    );
+}
